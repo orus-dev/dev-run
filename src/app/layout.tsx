7fb3fb23bb1640eb1de557e31d27909e8d7160d2
Leo dev <klesti.selimay@icloud.com>
2026-02-02 17:11:06 +0100

    dynamic Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,7 @@ export default function RootLayout({
       >
         <div className="min-h-screen flex flex-col">
           <Navbar />
-          <main className="flex-1">
-            {children}
-          </main>
+          <main className="flex-1">{children}</main>
           <Footer />
         </div>
       </body>
