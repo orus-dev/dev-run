231e9d57c8feebc5e3ac6a649accf71b91e53a1e
Leo dev <klesti.selimay@icloud.com>
2026-02-05 15:29:16 +0100

    Sign in with github implementation

--- src/modules/account/actions.ts
+++ src/modules/account/actions.ts
@@ -1,3 +1,26 @@
 "use server";
 
 import * as Core from "./core";
+import { createClient } from "@/lib/supabase";
+import { redirect } from "next/navigation";
+
+export async function signInWithGitHub() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+  });
+
+  if (error) {
+    console.error(error);
+    return redirect("/login?msg=Trouble");
+  }
+
+  return redirect(data.url);
+}
+
+export async function signOut() {
+  const supabase = await createClient();
+
+  const { error } = await supabase.auth.signOut();
+}
