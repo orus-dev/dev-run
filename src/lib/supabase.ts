0e3663714e9c28a49f430ca879e7b08c86f329ab
Leo dev <klesti.selimay@icloud.com>
2026-02-05 14:08:31 +0100

    Supabase implementation

---
+++ src/lib/supabase.ts
@@ -0,0 +1,26 @@
+import "server-only";
+
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SECRET_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set(name, value, options);
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set(name, "", options);
+        },
+      },
+    },
+  );
+}
