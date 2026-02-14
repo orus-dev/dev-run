204f3754f29e9c867eca0cbbf99b41eedaebeb88
Klesti Selimaj <klesti.selimay@icloud.com>
2026-02-14 23:25:28 +0100

    Finally fixed origin

--- src/lib/origin/server.ts
+++ src/lib/origin/server.ts
@@ -4,7 +4,11 @@ import "server-only";
 export async function getOrigin(): Promise<{ protocol: string; host: string }> {
   const h = await headers();
   const host = process.env.ORIGIN || h.get(":authority") || "localhost:3000";
-  const protocol = h.get("x-forwarded-proto") || h.get(":scheme") || "https";
+  const protocol =
+    process.env.ORIGIN_PROTO ||
+    h.get("x-forwarded-proto") ||
+    h.get(":scheme") ||
+    "https";
 
   return { protocol, host };
 }
