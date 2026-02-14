137d263060e40d8dcd718c321e8f312ea0b1af80
Klesti Selimaj <klesti.selimay@icloud.com>
2026-02-14 22:45:40 +0100

    Testing redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,11 +1,7 @@
 import "server-only";
 import Redis from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL!, {
-  tls: {
-    rejectUnauthorized: true,
-  },
-});
+export const redis = new Redis(process.env.REDIS_URL!);
 
 redis.on("error", (err) => {
   console.error("Redis error:", err);
