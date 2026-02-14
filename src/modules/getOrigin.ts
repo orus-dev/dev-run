694652dccd53e2cb96086de3e83b8d14478946a5
Klesti Selimaj <klesti.selimay@icloud.com>
2026-02-14 23:07:55 +0100

    Fixed origin

--- src/modules/getOrigin.ts
+++ src/modules/getOrigin.ts
@@ -4,8 +4,8 @@ import { headers } from "next/headers";
 
 export async function getOrigin(): Promise<{ protocol: string; host: string }> {
   const h = await headers();
-  const host = h.get("host") || "localhost:3000";
-  const protocol = h.get("x-forwarded-proto") ?? "https";
+  const host = process.env.ORIGIN || h.get(":authority") || "localhost:3000";
+  const protocol = h.get("x-forwarded-proto") || h.get(":scheme") || "https";
 
   return { protocol, host };
 }
